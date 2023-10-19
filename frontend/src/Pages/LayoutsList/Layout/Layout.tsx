4c0c402c899927c9e20dbea0654b7494710fef09
Danek <danek.fill@outlook.com>
2023-10-19 14:19:08 -0400

    adjust layout pages

--- frontend/src/Pages/LayoutsList/Layout/Layout.tsx
+++ frontend/src/Pages/LayoutsList/Layout/Layout.tsx
@@ -10,9 +10,15 @@ type LayoutProps = {
 const Banner: React.FC<LayoutProps> = ({layout }) => {
 
   return(
-    <div className="banner full">
+    <div className="banner fullWidth">
       <div className="inside">
-        {layout.name}
+        <h1 className="title">{layout.name}</h1>
+        <div className="monitors">Monitors layout goes here</div>
+        <div className="description">Short <i>optional</i> description.</div>
+        <div className="edit"><button className="edit">Edit Button</button></div>
+      </div>
+      <div className="rightSide">
+        <button className="apply">Apply Layout.</button>
       </div>
     </div>
   )
